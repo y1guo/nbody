ba9b8de3082f1371cce16fb1a83ed10a443c7f3c
Yi Guo <guoyi0328@gmail.com>
2022-03-11 00:45:17 -0800

    update find-initial-conditiion

--- antennae/find-initial-condition.py
+++ antennae/find-initial-condition.py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 import os
 import sys
 
@@ -5,27 +6,45 @@ directory = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(directory)
 sys.path.append(parent_directory)
 from leapfrog import NBodySystem
-import numpy as np
+import math
 
 n = 2
-m = np.full(n, 1.25).reshape((n, 1))
-x = np.zeros((n, 3))
-v = np.zeros((n, 3))
+m = [1.25, 1.25]
+x = [[0] * 3 for _ in range(n)]
+v = [[0] * 3 for _ in range(n)]
 t = 0
-dt = 0.01
+dt = -0.0001
+nstep = 100
 
-x[0, 0] = -1.5
-x[1, 0] = 1.5
-v[0, 1] = -((1.25 / 12) ** 0.5)
-v[1, 1] = (1.25 / 12) ** 0.5
+x[0][0] = 0.25
+x[1][0] = -0.25
+v[0][1] = math.sqrt(1.25 / 6) * 3
+v[1][1] = -math.sqrt(1.25 / 6) * 3
 
 nbody = NBodySystem(n, m, x, v, t)
 
-for _ in range(1200):
+px = [[] for _ in range(3)]
+
+for _ in range(10000):
     nbody.leapfrog(dt)
-    for i in range(n):
-        print(
-            "{:>8.2f}{:>4d}{:>8.2f}{:>8.2f}{:>8.2f}{:>8.2f}{:>8.2f}{:>8.2f}".format(
-                nbody.time, i, *nbody.position[i], *nbody.velocity[i]
-            ),
-        )
+    if _ % nstep == 0:
+        for i in range(n):
+            # print(
+            #     "{:>8.2f}{:>4d}{:>8.2f}{:>8.2f}{:>8.2f}{:>8.2f}{:>8.2f}{:>8.2f}".format(
+            #         nbody.time, i, *nbody.position[i], *nbody.velocity[i]
+            #     ),
+            # )
+            print(nbody.time, i, *nbody.position[i], *nbody.velocity[i])
+            for j in range(3):
+                px[j].append(nbody.position[i][j])
+
+for i in range(n):
+    print(nbody.time, i, *nbody.position[i], *nbody.velocity[i])
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.scatter(px[0], px[1], s=1)
+plt.xlim([-0.75, 0.75])
+plt.ylim([-0.75, 0.75])
+plt.show()
